01db6e92909120f3a12443d4fccf4740a0a84ce4
callmetaka <erviana.205025@mhs.its.ac.id>
2022-02-09 22:58:56 +0700

    chore: modify package

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ['./src/**/*.{html,css,js}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'Poppins': ['"Poppins"'],
+      },
+      colors:{
+        'main': '#FE9E44',
+        'background': '#FFDCB7',
+      }
+    },
+  },
+  plugins: [],
+}
